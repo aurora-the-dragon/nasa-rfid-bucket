11fb61a2ac3551e33eec26f5ddced6ee44adaf55
Jules <113810702+julia92122@users.noreply.github.com>
2025-04-15 16:46:12 -0400

    Update RFID_GUICounter.py
    fixed issues

--- RFID_GUICounter.py
+++ RFID_GUICounter.py
@@ -1,63 +1,63 @@
+import threading
+import select
+import sys
 import tkinter as tk
 from collections import deque
-import threading
+import time
 
 def recieve_tag_data():
     global tags
-
     while True:
         tag = input() 
+        
+        global tag_time
+        tag_time = time.time()
+
         for i in range(len(tag) // 24):
             tag = tag[i*24:(i+1)*24][18:24]  # Extract the last 6 characters of each 24-character RFID tag
-           # if len(tags) >= 50:
-            get_water_level(tag)
-            counter()
-           #     tags.popleft()  # keep buffer of 50 tags
+            if len(tags) >= 25:
+                tags.popleft()  # keep buffer of 100 tags
             tags.append(tag)
-        tags.clear()
 
-def get_water_level(tag):
-    # Use results of tags to determine water level
-    global waterLevel
+def get_water_level():
+    while True:
+        time.sleep(1)  # sleep for 1 s to prevent CPU overload
+        if 'ECAE34' in tags:
+            waterLevel = str(0)
+        elif 'ECB3CB' in tags:
+            waterLevel = str(25)
+        elif 'ECEFAB' in tags:
+            waterLevel = str(50)
+        elif 'ECDD59' in tags:
+            waterLevel = str(75)
+        elif 'EC3B21' and flag == 1:
+            waterLevel = str(100)
+        else: 
+            waterLevel = str(100)
 
-    if 'ECAE34' in tag:
-        waterLevel = str(10)
-    elif 'ECB3CB' in tag:
-        waterLevel = str(33)
-    elif 'ECEFAB' in tag:
-        waterLevel = str(66)
-    elif 'ECDD59' in tag:
-        waterLevel = str(99)
-    else: # experimenting with full tag
-        waterLevel = str(0)
-
-   # print("The water level is " + waterLevel + "%")
-   # clock.config(text=f"Water Level: {waterLevel}%")
-
-def counter():
-    global outlevel
-    global count
-    global waterLevel
-
-    if waterLevel <= outlevel:
-        outlevel = waterLevel
-        count = 0
-    elif count >= 50:
-        outlevel = waterLevel
-        count = 0
-    else:
-        count += 1
-
-    countstr = str(count)
-    print("count is " + countstr)
-    print("The water level is " + outlevel + "%")
-    print("The last read level is " + waterLevel + "%")
-    clock.config(text=f"Water Level: {outlevel}%")
+        print("The water level is " + waterLevel + "%")
+        clock.config(text=f"Water Level: {waterLevel}%")
 
 def start_rfid_thread():
     # seperate thread to process rfid tag info
-    thread = threading.Thread(target=recieve_tag_data, daemon=True)
-    thread.start()
+    thread1 = threading.Thread(target=recieve_tag_data, daemon=True)
+    thread1.start()
+    thread2 = threading.Thread(target=check_input_timeout, daemon=True)
+    thread2.start()
+    thread3 = threading.Thread(target=get_water_level, daemon=True)
+    thread3.start()
+
+def check_input_timeout():
+    global flag
+    global tag_time
+    tag_time = time.time()
+    while True:
+        time_check = time.time()
+        if time_check - tag_time > 10:
+            #print("No input received for 10 seconds, setting flag to 1")
+            flag = 1
+        else:
+            flag = 0  # reset flag if input is received
 
 # Initialize GUI
 root = tk.Tk()
@@ -65,10 +65,6 @@ root.geometry("300x150")
 root.title("Bucket Water Level")
 clock = tk.Label(root, text="Water Level: N/A", font=("Arial", 20), fg="blue")
 clock.pack(pady=20)
-
-outlevel = str(80)
-waterlevel = str(80)
-count = 0
 
 tags = deque()
 start_rfid_thread()
